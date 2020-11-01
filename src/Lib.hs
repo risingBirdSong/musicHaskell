17ec8ae1cd882a4161ca2c50bb5642f3b341c8e3
risingbirdsong <heregoingthere@outlook.com>
2020-10-31 21:39:02 -0700

    could not quickly restart when using ffplay but found flag -exitonkeydown and now it works much better by clicking any button to stop playing

--- src/Lib.hs
+++ src/Lib.hs
@@ -6,7 +6,8 @@ import qualified Data.ByteString.Lazy as B
 import qualified Data.ByteString.Builder as B
 import Data.List
 import Data.Foldable
-import qualified System.Process as SP
+import System.Process
+import Text.Printf
 volume = 0.5
 filename = "test.bin"
 wave :: [Float]
@@ -15,14 +16,16 @@ wave = map (* volume) $ map (sin) $ map (* step) [0.0..48000]
 
 built = B.toLazyByteString $ fold $ (map (B.floatLE) wave)
 
-save :: IO ()
-save = B.writeFile filename $ built
+save :: FilePath -> IO ()
+save filenm = B.writeFile filenm $ built
 
 -- command
 -- ffplay -f f32le -ar 48000 test.bin
 play :: IO ()
 play = do 
-    save 
+    save filename
+    _ <- runCommand $ printf "ffplay -f f32le -exitonkeydown -ar 48000 %s" filename
+    return ()
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
